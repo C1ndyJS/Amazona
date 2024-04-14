865f399b1a0a0b76ec91925d3fe66cb07c7d037b
Juan Felipe Agudelo <juanfe92013@gmail.com>
2024-04-14 15:15:17 -0500

    Database

---
+++ database/db.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('mysql://u41e0haegfc0fixl:HeLOR5M1R4Qyz9A6wCB@bofw568bn7fz0r9222jf-mysql.services.clever-cloud.com:21296/bofw568bn7fz0r9222jf')
+
+async function connectToDatabase() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+connectToDatabase();
