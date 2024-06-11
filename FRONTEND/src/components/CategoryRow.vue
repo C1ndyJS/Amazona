9ffc7a78012493dd170a50fa11a2ff6de990faf2
Cindy Marcela Jiménez Saldarriaga <64056282+C1ndyJS@users.noreply.github.com>
2024-06-11 22:50:53 +0000

    front components

---
+++ FRONTEND/src/components/CategoryRow.vue
@@ -0,0 +1,86 @@
+<script>
+export default {
+  props: {
+    categorias: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="box-row">
+    <div class="categories">
+        <div v-for="categoria in categorias" :key="categoria.id_categoria" class="card" style="width: 18rem;">
+          <router-link v-if="categoria.id_categoria" :to="{ name: 'home', params: { id: categoria.id_categoria } }" class="categoria-link">
+          <img :src="categoria.url_imagen" class="card-img-top" :alt="categoria.nombre" />
+            <div class="card-body">
+              <h5 class="card-title">{{ categoria.nombre }}</h5>
+              <p class="card-text">{{ categoria.descripcion }}</p>
+            </div>
+          </router-link>
+        </div>
+
+    </div>
+  </div>
+</template>
+
+<style>
+.box-row {
+  display: flex;
+  flex-wrap: wrap;
+  row-gap: 20px;
+  justify-content: space-between;
+  padding-bottom: 15px;
+}
+
+.card {
+  box-shadow: 5px 5px;
+}
+
+.categories {
+  padding-left: 20px;
+  padding-top: 20px;
+  gap: 40px;
+  display: flex;
+}
+
+
+.box-col {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 15px 20px;
+  background: #fff;
+  max-width: 24%;
+  min-height: 200px;
+  border-radius: 2%;
+  transition: box-shadow 0.3s ease;
+}
+.card-body h5{
+  color: black;
+}
+
+.card-body p {
+  color: black;
+}
+.card-img-top {
+  height: 180px;
+}
+
+.categoria-link {
+  text-decoration: none; /* Elimina el subrayado de los hipervínculos */
+}
+
+.categoria-link:hover .box-col {
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); /* Aplica sombra al pasar el mouse */
+}
+img.categorias {
+  width: 100%;
+  height: auto; /* Esto mantiene la proporción de aspecto */
+  border-radius: 5px;
+}
+
+</style>
+
---
+++ FRONTEND/src/components/ProductsRow.vue
@@ -0,0 +1,57 @@
+<script>
+export default {
+  props: {
+    productos: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="box-row">
+    <div v-for="(productos, index) in productos" :key="index" class="box-col">
+      <a href="'/FRONTEND/src/components/TestC.vue'" class="categoria-link">
+        <h3>{{ producto.nombre }}</h3>
+      <img :src="producto.url_imagen" class="productos"/>
+    </a>
+    </div>
+  </div>
+</template>
+
+<style>
+.box-row {
+  display: flex;
+  flex-wrap: wrap;
+  row-gap: 20px;
+  justify-content: space-between;
+}
+
+.box-col {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 15px 20px;
+  background: #fff;
+  max-width: 24%;
+  min-height: 200px;
+  border-radius: 2%;
+  transition: box-shadow 0.3s ease;
+}
+
+.categoria-link {
+  text-decoration: none; /* Elimina el subrayado de los hipervínculos */
+}
+
+.categoria-link:hover .box-col {
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); /* Aplica sombra al pasar el mouse */
+}
+img.categorias {
+  width: 100%;
+  height: auto; /* Esto mantiene la proporción de aspecto */
+  border-radius: 5px;
+}
+
+</style>
+
